fa173abe211cae682e8d963d70c23d195cc31a76
Lydia Rees <lydiairis76@gmail.com>
2020-10-16 19:27:23 -0500

    edit

--- src/components/additemform.jsx
+++ src/components/additemform.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import firebase from "firebase"
+import firebase from "./firebase"
 
 const AddItemForm = () => {
   //useState() hook captures the value from the input value
