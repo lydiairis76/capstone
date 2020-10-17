34168126ab1c647c4e194cfb56aa015938a9916d
Lydia Rees <lydiairis76@gmail.com>
2020-10-17 12:32:25 -0500

    API edit

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export function getWeather(zipCode) {
-    const endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},US&units=imperial&appid=c7c75b49b3c71ddf2f1a3baeaf301451`
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},US&units=imperial&appid=${process.env.GATSBY_WEATHER_API_KEY}`
     return axios.get(endpoint).then(response => response.data)
 }
